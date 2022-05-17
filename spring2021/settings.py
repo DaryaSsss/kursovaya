04c2ea532551051980c147fe8a647067cee1f008
DaryaSsss <d.semina10@gmail.com>
2022-05-17 16:26:15 +0300

    блок на 3

--- spring2021/settings.py
+++ spring2021/settings.py
@@ -68,6 +68,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'simple_history',
+    'django_filters',
 ]
 
 REST_FRAMEWORK = {
@@ -77,8 +78,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 3
-
+    'PAGE_SIZE': 3,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 
